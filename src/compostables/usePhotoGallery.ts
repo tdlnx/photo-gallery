0cd9f5122ef2d5a723fce62939c706f1cd16bd02
Travis Witt <travis_witt@sweetwater.com>
2022-03-11 13:28:44 -0500

    Added platform detection for mobile and web

--- src/compostables/usePhotoGallery.ts
+++ src/compostables/usePhotoGallery.ts
@@ -1,4 +1,6 @@
 import {ref, onMounted, watch} from 'vue';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
@@ -28,24 +30,41 @@ const convertBlobToBase64 = (blob: Blob) =>
 });
 
 const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+  let base64Data: string;
 
-  // Fetch the photo, read as a blob, then convert to base64
-  const response = await fetch(photo.webPath!);
-  const blob = await response.blob();
-  const base64Data = (await convertBlobToBase64(blob)) as string;
-
+  // Check platform - 'hybrid' is iOS or Android
+  if (isPlatform('hybrid')){
+    const file = await Filesystem.readFile({
+      path: photo.path!,
+    });
+    base64Data = file.data;
+  } else {
+    // Fetch the photo, read as a blob, then convert to base64
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    base64Data = (await convertBlobToBase64(blob)) as string; 
+  }
+   
   const savedFile = await Filesystem.writeFile({
     path: fileName,
     data: base64Data,
     directory: Directory.Data,
   });
-
-  // Display the photo using webpath rather than base64 since it's
-  // already in memory
-  return {
-    filepath: fileName,
-    webviewPath: photo.webPath,
-  };
+  
+  if (isPlatform('hybrid')) {
+  // Display new image by writing the 'file://' path to HTTP
+    return {
+      filepath: savedFile.uri,
+      webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+    };
+  } else {
+    // Display the photo using webpath rather than base64 since it's
+    // already in memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  }
 };
 
 // Load photos from storage
@@ -53,12 +72,15 @@ const loadSaved = async () => {
   const photoList = await Storage.get({key: PHOTO_STORAGE});
   const photosInStorage = photoList.value ? JSON.parse(photoList.value) : [];
 
-  for (const photo of photosInStorage){
-    const file = await Filesystem.readFile({
-      path: photo.filepath,
-      directory: Directory.Data,
-    });
-    photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+  // If running on the web...
+  if (!isPlatform('hybrid')) {
+    for (const photo of photosInStorage){
+      const file = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
   }
 
   photos.value = photosInStorage;
